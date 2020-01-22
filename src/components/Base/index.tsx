4f5cea535eee589f0659a435977d4dea8df83997
Dion Ridley <dridley@gmail.com>
2020-01-22 01:43:23 -0500

    Converted from FlatList back to ScrollView

--- src/components/Base/index.tsx
+++ src/components/Base/index.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 import { theme } from '../../themes';
 
-export const Page = styled.View`
+export const Page = styled.ScrollView`
   display: flex;
   flex-direction: column;
   flex: 1;
