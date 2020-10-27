a5d070e9817af12281e76d40e4ae1dc9a29bd435
Casey Rieder <616309-caseyrieder@users.noreply.gitlab.com>
2020-10-26 20:45:05 -0500

    color updating

--- src/components/MessageControl/color.tsx
+++ src/components/MessageControl/color.tsx
@@ -1,9 +1,10 @@
 import React, { Fragment, FC } from 'react';
+import { Button } from 'react-native';
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 import Slider from 'azir-slider';
 import { rgb2Hex, hex2Rgb } from '../../utils/color';
-import { theme, otherColors, height, width } from '../../themes';
+import { theme, height, width } from '../../themes';
 
 // import { theme } from '../../themes';
 // import { lang } from '../lang/en';
@@ -13,28 +14,21 @@ interface IProps {
   value: string;
 }
 
-interface ISliderProps {
-  gradient: string[];
-  val: number;
-  onChange: (val: number) => void;
-  color: string;
-}
-
 const Container = styled.TouchableOpacity`
   height: ${height * 0.045}px;
   width: 90%;
   margin-left: 2%;
   margin-top: ${height / 330}px;
 `;
-  
+
 const Box = styled.View`
-  margin-top: ${height*0.02}px;
+  margin-top: ${height * 0.02}px;
   margin-bottom: 15px;
   height: 28%;
   width: 100%;
   padding-horizontal: 5%;
   background-color: ${theme.colors.grey.light};
-`
+`;
 
 const Background = styled(LinearGradient)`
   position: absolute;
@@ -64,45 +58,10 @@ const gradGreen = [theme.colors.black.main, theme.colors.misc.green];
 const gradBlue = [theme.colors.black.main, theme.colors.misc.blue];
 
 export const Color: FC<IProps> = props => {
-  // for some reason hooks for state didn't work so we replaced with actual variables
-  // const [hue, setHue] = useState(0);
-  // let hue = 0;
-  // let saturation = 0;
-
   let RGB = hex2Rgb(props.value);
   let red = RGB[0];
-  let blue = RGB[1];
-  let green = RGB[2];
-
-  // const showColors = (value: number) => {
-  //   const colors = [
-  //     HSLToHex(value, 0.2, LIGHTNESS),
-  //     HSLToHex(value, 0.8, LIGHTNESS),
-  //   ];
-
-  //   return colors;
-  // };
-
-  // const getSaturation = () =>
-  //   SATURATION_MIN + (SATURATION_MAX - SATURATION_MIN) * (saturation / 100);
-
-  // const updateColor = () => {
-  //   console.log(`new color: [${red}, ${green}, ${blue}]`);
-  //   props.change(rgb2Hex([red, green, blue]));
-  // };
-  // const updateColor = (val: string) => {
-  //   props.change(val);
-  // };
-
-  // const changeHue = (value: number) => {
-  //   hue = value;
-  //   updateColor();
-  // };
-
-  // const changeColor = (value: number) => {
-  //   saturation = value;
-  //   updateColor();
-  // };
+  let green = RGB[1];
+  let blue = RGB[2];
 
   const updateColor = () => {
     props.change(rgb2Hex([red, green, blue]));
@@ -124,68 +83,6 @@ export const Color: FC<IProps> = props => {
     updateColor();
   };
 
-  // const changeG = (value: number) => {
-  //   RGB = [RGB[0], value, RGB[2]];
-  //   let newHex: string = rgb2Hex(RGB);
-  //   updateColor(newHex);
-  // };
-
-  // const changeB = (value: number) => {
-  //   RGB = [RGB[0], RGB[1], value];
-  //   let newHex: string = rgb2Hex(RGB);
-  //   updateColor(newHex);
-  // };
-
-  // const changeRGB = (value: number, color: string) => {
-  //   let newRgb: number[] = RGB;
-  //   if (color === 'r') {
-  //     // RGB = [value, green, blue];
-  //     newRgb[0] = value;
-  //   } else if (color === 'g') {
-  //     // RGB = [red, value, blue];
-  //     newRgb[1] = value;
-  //   } else {
-  //     // RGB = [red, green, value];
-  //     newRgb[2] = value;
-  //   }
-  //   return newRgb;
-  // };
-
-  // function changeColor(value: number, color: string) {
-  //   // const newRgb = await changeRGB(value, color);
-  //   // props.change(rgb2Hex(newRgb));
-  //   props.change(rgb2Hex(changeRGB(value, color)));
-  // }
-
-  // function updateRgb(vals: number[]) {
-  //   props.change(rgb2Hex(vals));
-  // }
-
-  // const RGBSlider: FC<ISliderProps> = sliderProps => (
-  //   // const {gradient, color, clr} = props;
-  //   // gradient: string[] = gradRed/Green/Blue
-  //   // color: string = otherColors.red/green/blue
-  //   // clr: string = 'r','g','b'
-  //   <Container>
-  //     <Background
-  //       start={{ x: 0, y: 0 }}
-  //       end={{ x: 1, y: 0 }}
-  //       colors={sliderProps.gradient}
-  //     />
-  //     <StyledSlider
-  //       value={sliderProps.val}
-  //       step={1}
-  //       minimumValue={0}
-  //       maximumValue={255}
-  //       minimumTrackTintColor="transparent"
-  //       maximumTrackTintColor="transparent"
-  //       thumbTintColor={sliderProps.color}
-  //       thumbSize={60}
-  //     />
-  //     <SectionLabel>{sliderProps.val}</SectionLabel>
-  //   </Container>
-  // );
-
   return (
     <Fragment>
       <Box>
@@ -203,7 +100,7 @@ export const Color: FC<IProps> = props => {
             maximumValue={255}
             thumbColor={theme.colors.misc.red}
             thumbSize={60}
-            onChange={(data: number) => changeRed(data)}
+            onValueChange={(data: number) => changeRed(data)}
           />
         </Container>
         <Container>
@@ -213,13 +110,13 @@ export const Color: FC<IProps> = props => {
             colors={gradGreen}
           />
           <StyledSlider
-            value={0}
+            value={green}
             step={1}
             minimumValue={0}
             maximumValue={255}
             thumbColor={theme.colors.misc.green}
             thumbSize={60}
-            onChange={(data: number) => changeGreen(data)}
+            onValueChange={(data: number) => changeGreen(data)}
           />
         </Container>
         <Container>
@@ -229,13 +126,13 @@ export const Color: FC<IProps> = props => {
             colors={gradBlue}
           />
           <StyledSlider
-            value={0}
+            value={blue}
             step={1}
             minimumValue={0}
             maximumValue={255}
             thumbColor={theme.colors.misc.blue}
             thumbSize={60}
-            onChange={(data: number) => changeBlue(data)}
+            onValueChange={(data: number) => changeBlue(data)}
           />
         </Container>
       </Box>
